6c44148eb18a40cb54c6bff22d6198b01022377d
Robert <fatkhullin.robert@gmail.com>
2025-04-26 22:39:47 +0300

    Added tests

--- internal/service/previous_day_info/previous_day_info.go
+++ internal/service/previous_day_info/previous_day_info.go
@@ -38,7 +38,7 @@ type PreviousDayInfo struct {
 	APIClient              APIClient
 	tgTimeAggregatorClient aggregatorClient
 	notifier               notifier
-	clock                  *clockwork.Clock
+	clock                  clockwork.Clock
 	hour, minute, second   int
 }
 
@@ -50,6 +50,22 @@ func NewPreviousDayInfo(
 	c clockwork.Clock,
 	hour, minute, second int,
 ) (*PreviousDayInfo, error) {
+	if c == nil {
+		return nil, fmt.Errorf("click must be set")
+	}
+
+	if hour < 0 || hour > 23 {
+		return nil, fmt.Errorf("hour must be between 0 and 23")
+	}
+
+	if minute < 0 || minute > 59 {
+		return nil, fmt.Errorf("minute must be between 0 and 59")
+	}
+
+	if second < 0 || second > 59 {
+		return nil, fmt.Errorf("second must be between 0 and 59")
+	}
+
 	if nil == tgTimeAPIClient {
 		return nil, fmt.Errorf("APIClient must be set")
 	}
@@ -66,7 +82,7 @@ func NewPreviousDayInfo(
 		APIClient:              tgTimeAPIClient,
 		tgTimeAggregatorClient: aggregatorClient,
 		notifier:               notifier,
-		clock:                  &c,
+		clock:                  c,
 		hour:                   hour,
 		minute:                 minute,
 		second:                 second,
@@ -75,11 +91,12 @@ func NewPreviousDayInfo(
 
 // Start ...
 func (pdi *PreviousDayInfo) Start(ctx context.Context) error {
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
 	go func() {
-		err := pdi.everyDayByHour(ctx, pdi.sendAllUsersNotify, time.NewTicker(time.Minute))
-		if err != nil {
-			logger.Error("component", "previous_day_info", "during", "send notify", "error", err)
-		}
+		_ = pdi.everyDayByHour(ctx, pdi.sendAllUsersNotify, time.NewTicker(time.Minute))
 	}()
 
 	return nil
@@ -87,7 +104,7 @@ func (pdi *PreviousDayInfo) Start(ctx context.Context) error {
 
 func (pdi *PreviousDayInfo) everyDayByHour(
 	ctx context.Context,
-	handler func(ctx context.Context) error,
+	handler func(ctx context.Context, date string) error,
 	ticker *time.Ticker,
 ) error {
 	defer ticker.Stop()
@@ -101,12 +118,16 @@ func (pdi *PreviousDayInfo) everyDayByHour(
 
 		select {
 		case <-ticker.C:
-			cc := *pdi.clock
+			cc := pdi.clock
 			h, m, s := cc.Now().Clock()
 			if h == pdi.hour && m == pdi.minute && s == pdi.second {
-				err := handler(ctx)
+				err := handler(ctx, pdi.getPreviousDate("Europe/Moscow").Format(time.DateOnly))
 				if err != nil {
-					logger.Error("error send all users notify", "err", err.Error())
+					logger.Error(
+						"component", "previous_day_info",
+						"during", "send notify",
+						"error", err.Error(),
+					)
 				}
 			}
 		case <-ctx.Done():
@@ -116,15 +137,12 @@ func (pdi *PreviousDayInfo) everyDayByHour(
 }
 
 // Run ...
-func (pdi *PreviousDayInfo) sendAllUsersNotify(ctx context.Context) error {
-	timeSummaryResponse, err := pdi.tgTimeAggregatorClient.GetTimeSummary(
-		ctx,
-		"",
-		pdi.getPreviousDate("Europe/Moscow").Format(time.DateOnly),
-	)
+func (pdi *PreviousDayInfo) sendAllUsersNotify(ctx context.Context, date string) error {
+	timeSummaryResponse, err := pdi.tgTimeAggregatorClient.GetTimeSummary(ctx, "", date)
 	if err != nil {
 		return fmt.Errorf("error getting time summary: %w", err)
 	}
+
 	if len(timeSummaryResponse.Summary) == 0 {
 		return nil
 	}
@@ -132,9 +150,12 @@ func (pdi *PreviousDayInfo) sendAllUsersNotify(ctx context.Context) error {
 	for _, summaryByUser := range timeSummaryResponse.Summary {
 		user, errGetUserByMACAddress := pdi.APIClient.GetUserByMacAddress(ctx, summaryByUser.MacAddress)
 		if errGetUserByMACAddress != nil {
-			// TODO: log error
+			logger.Warn(
+				"component", "previous_day_info",
+				"during", "send notify",
+				"error", errGetUserByMACAddress.Error(),
+			)
 			continue
-			//return fmt.Errorf("error getting user by telegram id: %w", err)
 		}
 
 		hours, minutes := helper.SecondsToHM(summaryByUser.GetSeconds())
--- internal/service/previous_day_info/previous_day_info_test.go
+++ internal/service/previous_day_info/previous_day_info_test.go
@@ -2,6 +2,7 @@ package previous_day_info
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -13,6 +14,11 @@ import (
 	"github.com/jonboulle/clockwork"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	pb "github.com/robertobadjio/tgtime-aggregator/pkg/api/time_v1"
+	pbapiv1 "github.com/robertobadjio/tgtime-api/api/v1/pb/api"
+	"github.com/robertobadjio/tgtime-notifier/internal/helper"
+	"github.com/robertobadjio/tgtime-notifier/internal/service/notifier/telegram"
 )
 
 func TestCancelContext(t *testing.T) {
@@ -28,26 +34,11 @@ func TestCancelContext(t *testing.T) {
 		notifierMock := NewNotifierMock(controller)
 		require.NotNil(t, notifierMock)
 
-		/*notifierMock.
-		SendPreviousDayInfoMessageMock.
-		Expect(minimock.AnyContext, telegram.ParamsPreviousDayInfo{}).
-		Return(nil)*/
-
-		//const macAddress string = "00:1b:63:84:45:e6"
-
 		aggregatorClientMock := NewAggregatorClientMock(controller)
 		require.NotNil(t, aggregatorClientMock)
-		/*aggregatorClientMock.
-		GetTimeSummaryMock.
-		Expect(minimock.AnyContext, macAddress, "2025-05-06").
-		Return(&pb.GetSummaryResponse{}, nil)*/
 
 		apiClientMock := NewAPIClientMock(controller)
 		require.NotNil(t, apiClientMock)
-		/*apiClientMock.
-		GetUserByMacAddressMock.
-		Expect(minimock.AnyContext, macAddress).
-		Return(&pbapiv1.GetUserByMacAddressResponse{}, nil)*/
 
 		previousDayInfoService, err := NewPreviousDayInfo(
 			apiClientMock,
@@ -61,7 +52,7 @@ func TestCancelContext(t *testing.T) {
 		require.Nil(t, err)
 		require.NotNil(t, previousDayInfoService)
 
-		handler := func(_ context.Context) error {
+		handler := func(_ context.Context, _ string) error {
 			return nil
 		}
 
@@ -76,7 +67,7 @@ func TestTick(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	t.Cleanup(cancel)
 
-	t.Run("context cancelled", func(t *testing.T) {
+	t.Run("handle function in defined time", func(t *testing.T) {
 		innerCtx, innerCancel := context.WithCancel(ctx)
 		controller := minimock.NewController(t)
 
@@ -106,7 +97,7 @@ func TestTick(t *testing.T) {
 
 		const resultText string = "handler called"
 
-		handler := func(_ context.Context) error {
+		handler := func(_ context.Context, _ string) error {
 			fmt.Print(resultText)
 			return nil
 		}
@@ -139,4 +130,173 @@ func captureOutput(f func()) string {
 	_ = w.Close()
 	out, _ := io.ReadAll(r)
 	return string(out)
+}
+
+var expectedTimeSummaryByUsers = []*pb.Summary{
+	0: {
+		MacAddress:   "00:1b:63:84:45:e6",
+		Seconds:      123123,
+		Breaks:       nil,
+		Date:         "2025-05-06",
+		SecondsStart: 1746548943,
+		SecondsEnd:   1746548950,
+	},
+}
+
+func Test_sendAllUsersNotify(t *testing.T) {
+	t.Parallel()
+
+	controller := minimock.NewController(t)
+
+	tests := map[string]struct {
+		notifier         func() notifier
+		aggregatorClient func() aggregatorClient
+		APIClient        func() APIClient
+		date             string
+
+		expectedErr error
+	}{
+		"error get time summary for all users": {
+			notifier: func() notifier {
+				notifierMock := NewNotifierMock(controller)
+				require.NotNil(t, notifierMock)
+
+				return notifierMock
+			},
+			aggregatorClient: func() aggregatorClient {
+				aggregatorClientMock := NewAggregatorClientMock(controller)
+				require.NotNil(t, aggregatorClientMock)
+
+				aggregatorClientMock.
+					GetTimeSummaryMock.
+					Times(1).
+					Expect(minimock.AnyContext, "", "2025-05-06").
+					Return(nil, errors.New("internal error"))
+
+				return aggregatorClientMock
+			},
+			APIClient: func() APIClient {
+				apiClientMock := NewAPIClientMock(controller)
+				require.NotNil(t, apiClientMock)
+
+				return apiClientMock
+			},
+			date: "2025-05-06",
+
+			expectedErr: fmt.Errorf("error getting time summary: %w", errors.New("internal error")),
+		},
+		"empty time summary by all users": {
+			notifier: func() notifier {
+				notifierMock := NewNotifierMock(controller)
+				require.NotNil(t, notifierMock)
+
+				return notifierMock
+			},
+			aggregatorClient: func() aggregatorClient {
+				aggregatorClientMock := NewAggregatorClientMock(controller)
+				require.NotNil(t, aggregatorClientMock)
+
+				aggregatorClientMock.
+					GetTimeSummaryMock.
+					Times(1).
+					Expect(minimock.AnyContext, "", "2025-05-06").
+					Return(&pb.GetSummaryResponse{Summary: []*pb.Summary{}}, nil)
+
+				return aggregatorClientMock
+			},
+			APIClient: func() APIClient {
+				apiClientMock := NewAPIClientMock(controller)
+				require.NotNil(t, apiClientMock)
+
+				return apiClientMock
+			},
+			date: "2025-05-06",
+
+			expectedErr: nil,
+		},
+		"success send notify": {
+			notifier: func() notifier {
+				notifierMock := NewNotifierMock(controller)
+				require.NotNil(t, notifierMock)
+
+				notifierMock.
+					SendPreviousDayInfoMessageMock.
+					Expect(minimock.AnyContext, telegram.ParamsPreviousDayInfo{
+						TelegramID:   12312312,
+						SecondsStart: helper.SecondsToTime(1746548943),
+						SecondsEnd:   helper.SecondsToTime(1746548950),
+						Hours:        34,
+						Minutes:      12,
+						Breaks:       "",
+					}).
+					Return(nil)
+
+				return notifierMock
+			},
+			aggregatorClient: func() aggregatorClient {
+				aggregatorClientMock := NewAggregatorClientMock(controller)
+				require.NotNil(t, aggregatorClientMock)
+
+				aggregatorClientMock.
+					GetTimeSummaryMock.
+					Times(1).
+					Expect(minimock.AnyContext, "", "2025-05-06").
+					Return(&pb.GetSummaryResponse{Summary: expectedTimeSummaryByUsers}, nil)
+
+				return aggregatorClientMock
+			},
+			APIClient: func() APIClient {
+				apiClientMock := NewAPIClientMock(controller)
+				require.NotNil(t, apiClientMock)
+
+				apiClientMock.
+					GetUserByMacAddressMock.
+					Times(1).
+					Expect(minimock.AnyContext, "00:1b:63:84:45:e6").
+					Return(&pbapiv1.GetUserByMacAddressResponse{
+						User: &pbapiv1.User{
+							Id:         1,
+							Name:       "Ivan",
+							Surname:    "Ivanov",
+							Lastname:   "Ivanovich",
+							BirthDate:  "2020-03-01",
+							Email:      "ivan@gmail.com",
+							MacAddress: "00:1b:63:84:45:e6",
+							TelegramId: 12312312,
+							Role:       "employee",
+							Department: 1,
+							Position:   "programmer",
+						},
+					}, nil)
+
+				return apiClientMock
+			},
+			date: "2025-05-06",
+
+			expectedErr: nil,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			previousDayInfoService, err := NewPreviousDayInfo(
+				test.APIClient(),
+				test.aggregatorClient(),
+				test.notifier(),
+				clockwork.NewFakeClock(),
+				12,
+				0,
+				0,
+			)
+			require.Nil(t, err)
+			require.NotNil(t, previousDayInfoService)
+
+			err = previousDayInfoService.sendAllUsersNotify(minimock.AnyContext, test.date)
+			if test.expectedErr != nil {
+				assert.Error(t, test.expectedErr, err)
+			}
+		})
+	}
 }
